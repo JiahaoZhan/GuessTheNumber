44f21aac0955bbbcc07d9d81dad2a6e91862e699
JiahaoZhan <Ichirojp201@gmail.com>
2021-02-09 14:03:15 -0500

    Update script.js

--- Modal/script.js
+++ Modal/script.js
@@ -4,22 +4,24 @@ const modal = document.querySelector('.modal');
 const btns = document.querySelectorAll('.show-modal');
 const overlay = document.querySelector('.overlay');
 
-for (let btn of btns) {
-  btn.addEventListener('click', function () {
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
-}
-
 const closeModal = function () {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
 };
 
-document.querySelector(".close-modal").addEventListener("click", function () {
-  closeModal();
-});
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
-overlay.addEventListener("click", function () {
-  closeModal();
+for (let btn of btns) {
+  btn.addEventListener('click', openModal);
+}
+
+document.querySelector('.close-modal').addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
 });
