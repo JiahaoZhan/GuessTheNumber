8d0b777c46cfe65cca9073584aeb2b6be2b19f0a
JiahaoZhan <Ichirojp201@gmail.com>
2021-02-09 13:04:41 -0500

    Update script.js

--- Modal/script.js
+++ Modal/script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const btns = document.querySelectorAll('.show-modal');
+const overlay = document.querySelector('.overlay');
+
+for (let btn of btns) {
+  btn.addEventListener('click', function () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+}
+
+document.querySelector('.close-modal').addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
+
+overlay.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
